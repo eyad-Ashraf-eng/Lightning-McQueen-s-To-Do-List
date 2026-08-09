929d90b8963ba3f640b247086cb11a952f7ff596
Eyad Ashraf <eyadashraf353@gmail.com>
2026-08-10 02:25:57 +0300

    cases 1,2,3

--- Task.py
+++ Task.py
@@ -1,16 +1,27 @@
 def add_task(task, filename):
     with open(filename, "a") as f:
         f.write(task + "\n")
+        print("Task added")
 
-print("Greetings! Lightning McQueen is ready to race!")
-print("Here is your options")
-print("1- Add a task")
-print("2- View Current to-do list")
-print("3- Mark a task as complete")
-print("4- Remove a task")
-print("5- Quit")
-x = input("Please select an option: ")
-match x:
-    case "1":
-        task = input("Enter the task you want to add: ")
-        add_task(task, "tasks.txt")
+def view_tasks(filename):
+    with open(filename, "r")as f:
+        content = f.read()
+        print(content)      
+while True:
+    print("Greetings! Lightning McQueen is ready to race!")
+    print("Here is your options")
+    print("1- Add a task")
+    print("2- View Current to-do list")
+    print("3- Mark a task as complete")
+    print("4- Remove a task")
+    print("5- Quit")
+    x = input("Please select an option: ")
+    match x:
+     case "1":
+            task = input("Enter the task you want to add: ")
+            add_task(task, "tasks.txt")
+     case "2":
+            view_tasks("tasks.txt")
+     case "5":
+            print("Goodbye!")
+            break       
